22c974f2ca928b6b2f1b09102a940dcbf67cccda
chrispfae <christian.pfaendner@simtech.uni-stuttgart.de>
2022-10-31 15:01:50 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+        setup(long_description=long_description, long_description_content_type="text/markdown")
