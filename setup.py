57ba6653d788ededb1566082f078999f5d01e849
ProbstenHias <matthias.weilinger@gmx.de>
2022-11-01 12:18:09 +0100

    Implement Exercise: Packaging Python Code

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+import setuptools
+
+if __name__ == "__main__":
+    setup(url='https://github.com/ProbstenHias/diffusion2d',
+          author='Matthias Weilinger',
+          author_email='st156232@stud.uni-stuttgart.de',
+          )
