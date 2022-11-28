6725e53345f35d11ea3359e5b2f202ae9293b401
ProbstenHias <matthias.weilinger@gmx.de>
2022-11-28 20:53:21 +0100

    Add project name to setup.py as well

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 import setuptools
 
 if __name__ == "__main__":
-    setup(url='https://github.com/Simulation-Software-Engineering/diffusion2d',
+    setup(name='weilinms-diffusion2d',
+          url='https://github.com/Simulation-Software-Engineering/diffusion2d',
           author='Matthias Weilinger',
           author_email='st156232@stud.uni-stuttgart.de',
           )
